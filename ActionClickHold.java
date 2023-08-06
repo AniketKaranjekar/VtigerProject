1ad89ef840f1e9ece1100f27223d8ed98dc5e8a8
AniketKaranjekar <134379871+AniketKaranjekar@users.noreply.github.com>
2023-08-06 15:05:02 +0530

    Update ActionClickHold.java

--- ActionClickHold.java
+++ ActionClickHold.java
@@ -1,5 +1,4 @@
 package SeleniumTutorials;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,8 +9,8 @@ public class ActionClickHold
 {
 	public static void main(String[]args)
 	{
-		System.setProperty("webdriver.edge.driver","C:\\Users\\mansi computer\\Downloads\\edgedriver_win64\\edgedriver\\msedgedriver.exe");
-	    WebDriver driver =new EdgeDriver();
+	System.setProperty("webdriver.edge.driver","C:\\Users\\mansi computer\\Downloads\\edgedriver_win64\\edgedriver\\msedgedriver.exe");
+	WebDriver driver =new EdgeDriver();
         driver.get("http://www.dhtmlgoodies.com/submitted-scripts/i-google-like-drag-drop/index.html");
         WebElement source=driver.findElement(By.xpath("//h1[text()='Block 1']"));
         WebElement destination=driver.findElement(By.id("column-3"));
